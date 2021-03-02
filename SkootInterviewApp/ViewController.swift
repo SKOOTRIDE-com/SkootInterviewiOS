e64c6753a659fe4491325ccee6ab7d5e01c58ee7
Summer Jones <summer@skootride.com>
2021-03-02 17:26:57 +0000

    Initial Commit

---
+++ SkootInterviewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SkootInterviewApp
+//
+//  Created by Summer Jones on 02/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
