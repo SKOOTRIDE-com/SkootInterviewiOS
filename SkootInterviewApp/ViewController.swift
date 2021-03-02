f2b421adcfdf0fe72216e84f19dce141f9e41da0
Summer Jones <summer@skootride.com>
2021-03-02 17:56:15 +0000

    A basic app that shows a list of names with no architecture

--- SkootInterviewApp/ViewController.swift
+++ SkootInterviewApp/ViewController.swift
@@ -7,11 +7,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct User: Codable {
+    let id: String
+    let firstName: String
+}
 
+struct UserData: Codable {
+    let data: [User]
+}
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    var users: [User] = []
+
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
+
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+
+        let url = URL(string: "https://dummyapi.io/data/api/user")!
+        var request = URLRequest(url: url)
+        request.setValue("603e78921e1a8f012a249693", forHTTPHeaderField: "app-id")
+        let task = URLSession.shared.dataTask(with: request) {(data, response, error) in
+            guard let data = data else { return }
+            let users = try? JSONDecoder().decode(UserData.self, from: data)
+            self.users = users?.data ?? []
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+
+        task.resume()
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = users[indexPath.item].firstName
+        return cell!
     }
 
 
